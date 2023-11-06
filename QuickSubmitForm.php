86d52096b4d73c5fd395c48ab0f0c31cc19824f8
Rob Arnold <rob.arnold@ubiquitypress.com>
2023-11-06 13:50:12 -0300

    setting workflow stage ti copy editing rather than production

--- QuickSubmitForm.php
+++ QuickSubmitForm.php
@@ -382,7 +382,7 @@ class QuickSubmitForm extends Form {
 		}
 
 		$this->_submission->setLocale($this->getData('locale'));
-		$this->_submission->setStageId(WORKFLOW_STAGE_ID_PRODUCTION);
+		$this->_submission->setStageId(WORKFLOW_STAGE_ID_EDITING);
 		$this->_submission->setDateSubmitted(Core::getCurrentDate());
 		$this->_submission->setSubmissionProgress(0);
 
